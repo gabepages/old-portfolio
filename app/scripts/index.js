2cf047cdb4cc52d40fde62f2eef1f2b230c9d1d6
Gabriel Pages <gbpages@yahoo.com>
2016-04-25 22:30:27 -0400

    added portfolio header and page scrolling

---
+++ app/scripts/index.js
@@ -0,0 +1,10 @@
+var $ = require('jquery');
+var _ = require('underscore');
+var fullpage = require('fullpage.js');
+
+
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+       anchors:['home', 'about']
+    });
+});
