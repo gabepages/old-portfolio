65f7c87522efadb82da23a2614fef3f9d1c464d4
Gabriel Pages <gbpages@yahoo.com>
2016-04-26 22:50:02 -0400

    added social tabs

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,9 +2,9 @@ var $ = require('jquery');
 var _ = require('underscore');
 var fullpage = require('fullpage.js');
 
-
-$(document).ready(function() {
-    $('#fullpage').fullpage({
-       anchors:['home', 'about']
-    });
-});
+// 
+// $(document).ready(function() {
+//     $('#fullpage').fullpage({
+//        anchors:['home', 'about']
+//     });
+// });
