885be3de7f823744c2399d4dea927f15fe980a12
Gabriel Pages <gbpages@yahoo.com>
2016-04-28 02:21:40 -0400

    added scrolling feature and dynamic chevron also finished about page

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,10 +1,32 @@
 var $ = require('jquery');
 var _ = require('underscore');
-var fullpage = require('fullpage.js');
 
-// 
-// $(document).ready(function() {
-//     $('#fullpage').fullpage({
-//        anchors:['home', 'about']
-//     });
-// });
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 800);
+        return false;
+      }
+    }
+  });
+
+
+  var topOfSection = $("#topOfSection").offset().top;
+  $(window).scroll(function() {
+      if($(window).scrollTop() >= topOfSection) { //scrolled past the other div?
+        $(".hidden-top-button").removeClass('fadeOut');
+          $(".hidden-top-button").removeClass('hidden'); //reached the desired point -- show div
+      }else if($(window).scrollTop() < topOfSection){
+        $(".hidden-top-button").addClass('fadeOut');
+
+      }
+  });
+
+
+});
